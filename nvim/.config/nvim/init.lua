cb15c37d514289528614f55db01249f276fd9b25
nirimatt <matt_lucero@outlook.com>
2025-11-03 21:20:19 -0600

    add nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,2 @@
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
